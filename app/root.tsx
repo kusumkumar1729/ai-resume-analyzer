13663d7402b7a3be56d5b5ba19bdecf7f5a7aafd
Kusum462 <kusumakumarproject@.com>
2025-08-21 20:22:14 +0530

    Second Commit

--- app/root.tsx
+++ app/root.tsx
@@ -7,8 +7,11 @@ import {
   ScrollRestoration,
 } from "react-router";
 
+import {useEffect, useState} from "react";
+
 import type { Route } from "./+types/root";
 import "./app.css";
+import {usePuterStore} from "~/lib/puter";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -24,6 +27,15 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
+
+    const {init} = usePuterStore();
+
+    useEffect(()=>
+    {
+        init();
+
+    },[init])
+
   return (
     <html lang="en">
       <head>
@@ -32,7 +44,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
+
       <body>
+        <script src="https://js.puter.com/v2/"></script>
         {children}
         <ScrollRestoration />
         <Scripts />
